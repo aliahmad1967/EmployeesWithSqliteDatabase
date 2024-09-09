099b1e024fe662e88cd655787496b74fd8a743cc
nidal5nidal@gmail.com <nidal5nidal@gmail.com>
2024-09-09 12:38:30 +0300

    2nd

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -73,7 +73,7 @@
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.dtpDBO);
             this.panel1.Controls.Add(this.cboDep);
             this.panel1.Controls.Add(this.cboGender);
@@ -94,7 +94,7 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.txtEmpNo);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Font = new System.Drawing.Font("LBC", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.Location = new System.Drawing.Point(14, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1267, 196);
@@ -105,7 +105,7 @@
             // 
             this.dtpDBO.Location = new System.Drawing.Point(472, 150);
             this.dtpDBO.Name = "dtpDBO";
-            this.dtpDBO.Size = new System.Drawing.Size(265, 27);
+            this.dtpDBO.Size = new System.Drawing.Size(265, 24);
             this.dtpDBO.TabIndex = 11;
             // 
             // cboDep
@@ -130,7 +130,7 @@
             "الخدمات"});
             this.cboDep.Location = new System.Drawing.Point(485, 30);
             this.cboDep.Name = "cboDep";
-            this.cboDep.Size = new System.Drawing.Size(253, 28);
+            this.cboDep.Size = new System.Drawing.Size(253, 26);
             this.cboDep.TabIndex = 10;
             // 
             // cboGender
@@ -141,7 +141,7 @@
             "انثى"});
             this.cboGender.Location = new System.Drawing.Point(929, 113);
             this.cboGender.Name = "cboGender";
-            this.cboGender.Size = new System.Drawing.Size(188, 28);
+            this.cboGender.Size = new System.Drawing.Size(188, 26);
             this.cboGender.TabIndex = 10;
             // 
             // txtAddress
@@ -159,7 +159,7 @@
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(766, 34);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(51, 20);
+            this.label9.Size = new System.Drawing.Size(38, 18);
             this.label9.TabIndex = 4;
             this.label9.Text = "القسم";
             // 
@@ -168,7 +168,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(370, 122);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.Size = new System.Drawing.Size(50, 18);
             this.label5.TabIndex = 3;
             this.label5.Text = "العنوان";
             // 
@@ -177,7 +177,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(1145, 117);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(50, 20);
+            this.label6.Size = new System.Drawing.Size(44, 18);
             this.label6.TabIndex = 4;
             this.label6.Text = "الجنس";
             // 
@@ -186,7 +186,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(765, 153);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.Size = new System.Drawing.Size(82, 18);
             this.label4.TabIndex = 5;
             this.label4.Text = "تاريخ الميلاد";
             // 
@@ -195,7 +195,7 @@
             this.txtCardNo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtCardNo.Location = new System.Drawing.Point(472, 110);
             this.txtCardNo.Name = "txtCardNo";
-            this.txtCardNo.Size = new System.Drawing.Size(266, 27);
+            this.txtCardNo.Size = new System.Drawing.Size(266, 24);
             this.txtCardNo.TabIndex = 8;
             // 
             // label7
@@ -203,7 +203,7 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(765, 112);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(94, 20);
+            this.label7.Size = new System.Drawing.Size(82, 18);
             this.label7.TabIndex = 5;
             this.label7.Text = "الرقم الوطني";
             // 
@@ -212,7 +212,7 @@
             this.txtSeach.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtSeach.Location = new System.Drawing.Point(93, 32);
             this.txtSeach.Name = "txtSeach";
-            this.txtSeach.Size = new System.Drawing.Size(266, 27);
+            this.txtSeach.Size = new System.Drawing.Size(266, 24);
             this.txtSeach.TabIndex = 9;
             this.toolTip1.SetToolTip(this.txtSeach, "البحث في البيانات");
             this.txtSeach.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSeach_KeyPress);
@@ -222,7 +222,7 @@
             this.label10.AutoSize = true;
             this.label10.Location = new System.Drawing.Point(374, 34);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(45, 20);
+            this.label10.Size = new System.Drawing.Size(43, 18);
             this.label10.TabIndex = 6;
             this.label10.Text = "البحث";
             // 
@@ -231,7 +231,7 @@
             this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtPhone.Location = new System.Drawing.Point(472, 70);
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(266, 27);
+            this.txtPhone.Size = new System.Drawing.Size(266, 24);
             this.txtPhone.TabIndex = 9;
             // 
             // label8
@@ -239,7 +239,7 @@
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(765, 71);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(61, 20);
+            this.label8.Size = new System.Drawing.Size(55, 18);
             this.label8.TabIndex = 6;
             this.label8.Text = "الموبايل";
             // 
@@ -248,7 +248,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(1145, 161);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.Size = new System.Drawing.Size(51, 18);
             this.label3.TabIndex = 0;
             this.label3.Text = "الوظيفة";
             // 
@@ -257,7 +257,7 @@
             this.txtJob.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtJob.Location = new System.Drawing.Point(929, 154);
             this.txtJob.Name = "txtJob";
-            this.txtJob.Size = new System.Drawing.Size(189, 27);
+            this.txtJob.Size = new System.Drawing.Size(189, 24);
             this.txtJob.TabIndex = 1;
             // 
             // txtFullName
@@ -265,7 +265,7 @@
             this.txtFullName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtFullName.Location = new System.Drawing.Point(929, 70);
             this.txtFullName.Name = "txtFullName";
-            this.txtFullName.Size = new System.Drawing.Size(189, 27);
+            this.txtFullName.Size = new System.Drawing.Size(189, 24);
             this.txtFullName.TabIndex = 1;
             // 
             // label2
@@ -273,7 +273,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(1145, 77);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.Size = new System.Drawing.Size(80, 18);
             this.label2.TabIndex = 0;
             this.label2.Text = "الاسم الكامل";
             // 
@@ -282,7 +282,7 @@
             this.txtEmpNo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtEmpNo.Location = new System.Drawing.Point(1018, 31);
             this.txtEmpNo.Name = "txtEmpNo";
-            this.txtEmpNo.Size = new System.Drawing.Size(100, 27);
+            this.txtEmpNo.Size = new System.Drawing.Size(100, 24);
             this.txtEmpNo.TabIndex = 1;
             // 
             // label1
@@ -290,7 +290,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(1145, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.Size = new System.Drawing.Size(36, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "الرقم";
             // 
@@ -305,7 +305,7 @@
             this.panel2.Controls.Add(this.btnReset);
             this.panel2.Controls.Add(this.btnPrint);
             this.panel2.Controls.Add(this.btnAdd);
-            this.panel2.Font = new System.Drawing.Font("LBC", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel2.Location = new System.Drawing.Point(14, 214);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1267, 90);
@@ -384,7 +384,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel3.BackColor = System.Drawing.SystemColors.Control;
             this.panel3.Controls.Add(this.dataGridView1);
-            this.panel3.Font = new System.Drawing.Font("LBC", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel3.Location = new System.Drawing.Point(14, 310);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(1267, 326);
